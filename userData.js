34bc2fca146ec014b03090b302fefcdbf861b321
Moziezez <62847137+Moziezez@users.noreply.github.com>
2025-02-23 14:51:19 +0100

    userData.js included

---
+++ userData.js
@@ -0,0 +1,20 @@
+const userDataConst = {
+    name: 'Maurice',
+    date: '25.06.1997',
+    hour: '12',
+    minu: '25',
+    longi: '33',
+    lati: '22',
+    house: 'placidus',
+    city: 'Hildesheim',
+    country: 'Deutschland',
+    zodi: 'true',
+    check: 'false',
+    aspectChecks: [
+        '1', '1', '1', '1',
+        '1', '1', '1', '1',
+        '1', '1'
+    ]
+};
+
+export { userDataConst };
